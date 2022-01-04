a8980d8071094204d609dfcc6d22a13cb7a4f2b9
Harsh Mangalam <hmangalam@oplglobal.com>
2022-01-04 08:43:30 +0530

    speech to text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [content, setContent] = useState("");
+  const [lang, setLang] = useState("");
+
+  const handleSpeech2Text = () => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.lang = lang;
+    recognition.interimResults = false;
+    recognition.start();
+    recognition.onresult = (e) => {
+      const content = e.results[0][0].transcript;
+      setContent(content);
+    };
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleSpeech2Text}>Start</button>
+        <select value={lang} onChange={(e) => setLang(e.target.value)}>
+          <option value="en-IN">English India</option>
+          <option value="en-US">English US</option>
+          <option value="hi-IN">Hindi</option>
+        </select>
+        <div style={{ marginTop: 20 }}>{content}</div>
       </header>
     </div>
   );
